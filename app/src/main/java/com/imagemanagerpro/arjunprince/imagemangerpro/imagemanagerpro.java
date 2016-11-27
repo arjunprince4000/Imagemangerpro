21d9b286e1e0fc94a1c19aaae72f911745f83cd2
arjun prince <arjun prince>
2016-11-27 23:52:49 +0530

    Initial commit

---
+++ app/src/main/java/com/imagemanagerpro/arjunprince/imagemangerpro/imagemanagerpro.java
@@ -0,0 +1,13 @@
+package com.imagemanagerpro.arjunprince.imagemangerpro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class imagemanagerpro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imagemanagerpro);
+    }
+}
